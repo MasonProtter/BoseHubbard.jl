9b0befce98817e042a1d440bc84f1ace63aa73f7
MasonProtter <mason.protter@icloud.com>
2020-04-07 16:08:01 -0600

    add some comments

--- src/BoseHubbard.jl
+++ src/BoseHubbard.jl
@@ -6,9 +6,9 @@ using LinearAlgebra, Reexport
 export ⊗, ∑, up, dn, Bose_ladder, Hubbard_Hamiltonian, number_operator, Projector, number_projector
 export Hubbard_Hamiltonian_projected
 
-⊗(args...) = kron(args...)
+⊗(args...) = kron(args...) #Kronecker product
 
-const ∑ = sum
+const ∑ = sum  #fancy sum notation
 
 struct Up   end
 struct Dn end
@@ -17,34 +17,37 @@ const dn = Dn()
 
 
 function Bose_ladder(;L, Ncut)
-    id  = I(Ncut) |> BandedMatrix
-    id² = id ⊗ id 
-    a_local = BandedMatrix(-1 => sqrt.(Ncut-1:-1:1)) #diagm(-1 => sqrt.(Ncut-1:-1:1)) |> sparse
+    id  = I(Ncut) |> BandedMatrix # Spinless local identity operator
+    id² = id ⊗ id                # Spinful local identity operator 
+    a_local = BandedMatrix(-1 => sqrt.(Ncut-1:-1:1)) #Spinless lowering operator
 
-    a_local_up = a_local ⊗ id     
-    a_local_dn = id      ⊗ a_local
-    a(i, ::Up) = reduce(⊗, (i==j ? a_local_up : id² for j ∈ 1:L))
-    a(i, ::Dn) = reduce(⊗, (i==j ? a_local_dn : id² for j ∈ 1:L))
+    a_local_up = a_local ⊗ id       # Spin up lowering operator
+    a_local_dn = id      ⊗ a_local  # Spin down lowering operator
+    a(i, ::Up) = reduce(⊗, (i==j ? a_local_up : id² for j ∈ 1:L)) #up lowering operator at site i
+    a(i, ::Dn) = reduce(⊗, (i==j ? a_local_dn : id² for j ∈ 1:L)) #dn lowering operator at site i
 end
 
 function Hubbard_Hamiltonian(;U, μ, L, Ncut)
     a = Bose_ladder(;L=L, Ncut=Ncut)
-    n(i) = a(i, up)'a(i, up) + a(i, dn)'a(i, dn)
-    Hint(i) = sparse((U*n(i) - μ*I)*n(i))
-    ∑(1:L) do i
-        j = mod(i+1, 1:L) # periodic BCs
-        -1/2*((sparse(a(i, up)'a(j, up)) + hc) 
-              + (sparse(a(i, dn)'a(j, dn)) + hc)) + Hint(i)
-    end * inv(L) |> Hermitian
+    n(i) = a(i, up)'a(i, up) + a(i, dn)'a(i, dn) #local number operator
+    Hint(i) = sparse((U*n(i) - μ*I)*n(i))        #Interation term
+    
+    (1/L) * (Hermitian ∘ ∑)(1:L) do i  #sum over all sites
+        j = mod(i+1, 1:L)              # periodic BCs
+        Ht = -1/2*((sparse(a(i, up)'a(j, up)) + hc) 
+                   + (sparse(a(i, dn)'a(j, dn)) + hc)) #Kinetic term
+        Ht + Hint(i)
+    end
 end
 
+# Number operator for all sites
 function number_operator(;L, Ncut)
     a = Bose_ladder(;L=L, Ncut=Ncut)
     n(i) = (a(i, up)'a(i, up) + a(i, dn)'a(i, dn))
     ∑(n, 1:L)
 end
 
-
+#Projector onto a fixed particle number
 function number_projector(;L, Ncut)
     n̂ = number_operator(;L=L, Ncut=Ncut)
     function P(n)
@@ -56,29 +59,22 @@ function number_projector(;L, Ncut)
 end
 
 
-function Hubbard_Hamiltonian_projected(;U, μ, L, Ncut, n)
-    a = Bose_ladder(;L=L, Ncut=Ncut)
-    P = number_projector(;L=L, Ncut=Ncut)(n)
-    n̂(i) = P'*((a(i, up)'a(i, up)) + (a(i, dn)'a(i, dn)))*P
-    Hint(i) = (U*n̂(i) - μ*I)*n̂(i)
-    function Hkin(i, j)
-        aiupajup = (P'*(sparse(a(i, up)'a(j, up))))*P
-        aidnajdn = (P'*(sparse(a(i, dn)'a(j, dn))))*P
-        -((aiupajup + hc) + (aidnajdn + hc))
-    end
+# function Hubbard_Hamiltonian_projected(;U, μ, L, Ncut, n)
+#     a = Bose_ladder(;L=L, Ncut=Ncut)
+#     P = number_projector(;L=L, Ncut=Ncut)(n)
+#     n̂(i) = P'*((a(i, up)'a(i, up)) + (a(i, dn)'a(i, dn)))*P
+#     Hint(i) = (U*n̂(i) - μ*I)*n̂(i)
+#     function Hkin(i, j)
+#         aiupajup = (P'*(sparse(a(i, up)'a(j, up))))*P
+#         aidnajdn = (P'*(sparse(a(i, dn)'a(j, dn))))*P
+#         -((aiupajup + hc) + (aidnajdn + hc))
+#     end
     
-    ∑(1:L) do i
-        j = mod(i+1, 1:L) # periodic BCs
-        Hkin(i, j) + Hint(i)
-    end * inv(L) |> Hermitian
-end
-
-
-begin # piracy 
-    Base.:(+)(A::Hermitian, B::SparseMatrixCSC) = parent(A) + B
-    Base.:(+)(A::SparseMatrixCSC, B::Hermitian) = A + parent(B)
-    Base.:(+)(A::Hermitian{SparseMatrixCSC}, B::Hermitian{SparseMatrixCSC}) = Hermitian(parent(A) + parent(B))
-end
+#     ∑(1:L) do i
+#         j = mod(i+1, 1:L) # periodic BCs
+#         Hkin(i, j) + Hint(i)
+#     end * inv(L) |> Hermitian
+# end
 
 struct Projector 
     inds::Vector{Int} 
@@ -103,6 +99,14 @@ function Base.:(*)(PdgM::AdjointProjectorTimesMatrix{<:BandedMatrix}, P::Project
     @assert M.l == M.u == 0
     @inbounds Diagonal([M[i, i] for i ∈ PdgM.inds])
 end
+
+
+begin # piracy 
+    Base.:(+)(A::Hermitian, B::SparseMatrixCSC) = parent(A) + B
+    Base.:(+)(A::SparseMatrixCSC, B::Hermitian) = A + parent(B)
+    Base.:(+)(A::Hermitian{SparseMatrixCSC}, B::Hermitian{SparseMatrixCSC}) = Hermitian(parent(A) + parent(B))
+end
+
 
 
 end # module
